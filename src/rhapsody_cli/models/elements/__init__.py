b80127f6b19d171437c4fc2d8bec271ff3e04580
uie54187 <lei.23.zhang@continental-corporation.com>
2026-07-09 10:17:01 +0800

    feat: add leaf wrappers (relations + misc)
    - Add RPGeneralization (extends RPModelElement)
    - Add RPDependency (extends RPModelElement)
    - Add RPHyperLink (extends RPDependency)
    - Add RPAssociationRole (extends RPInstance)
    - Add RPComment (extends RPModelElement)
    - Add RPConstraint (extends RPModelElement)
    - Add RPEnumerationLiteral (extends RPModelElement)
    
    All wrappers registered with wrapper registry and fully tested
    14 tests pass (2 per wrapper)

--- src/rhapsody_cli/models/elements/__init__.py
+++ src/rhapsody_cli/models/elements/__init__.py
@@ -3,6 +3,7 @@
 from rhapsody_cli.models.elements import classifiers as classifiers  # noqa: F401
 from rhapsody_cli.models.elements import containment as containment  # noqa: F401
 from rhapsody_cli.models.elements import diagrams as diagrams  # noqa: F401
+from rhapsody_cli.models.elements import misc as misc  # noqa: F401
 from rhapsody_cli.models.elements import relations as relations  # noqa: F401
 from rhapsody_cli.models.elements import requirements as requirements  # noqa: F401
 from rhapsody_cli.models.elements import variables as variables  # noqa: F401
